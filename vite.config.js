093cac4fdaa6f553bf086c2e0a741307f65f01b8
Sunil Dharawat <Sun2001@sysassist.local>
2025-11-27 11:52:46 +0530

    Vite.config file cahnges

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Sysassist-React-Website/',
 
   // server :{
   //   host:"192.168.1.17",
